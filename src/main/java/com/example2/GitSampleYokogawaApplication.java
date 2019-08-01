e47c43aa10843ed8a34b808a0ef6e8dd9f09f596
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-08-01 14:49:09 +0900

    新規にプロジェクトを作成 first commit

---
+++ src/main/java/com/example2/GitSampleYokogawaApplication.java
@@ -0,0 +1,13 @@
+package com.example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYokogawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYokogawaApplication.class, args);
+	}
+
+}
